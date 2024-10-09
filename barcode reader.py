9a4f9caa6f414c3d2a76c6357b3005c9edeea108
saggatbob556 <saggatbob556@gmail.com>
2024-10-08 21:12:23 -0700

    Update barcode reader.py

--- barcode reader.py
+++ barcode reader.py
@@ -1,12 +1,15 @@
+# barcode lookup license: "9anv0b5gk9pon5ihsbgbxafo29mi7f"
 # barcode license: "DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAzMjkwOTIzLVRYbFFjbTlxIiwibWFpblNlcnZlclVSTCI6Imh0dHBzOi8vbWRscy5keW5hbXNvZnRvbmxpbmUuY29tIiwib3JnYW5pemF0aW9uSUQiOiIxMDMyOTA5MjMiLCJzdGFuZGJ5U2VydmVyVVJMIjoiaHR0cHM6Ly9zZGxzLmR5bmFtc29mdG9ubGluZS5jb20iLCJjaGVja0NvZGUiOjE4NDc0MTg1Mjl9"
+from __future__ import print_function
 import numpy as np
-import cv2 as cv
-    
+import cv2 as cv 
 from multiprocessing.pool import ThreadPool
-from collections import deque
-    
+from collections import deque  
 import dbr
 from dbr import *
+import urllib.request
+import json
+import pprint
 
 
 BarcodeReader.init_license("DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAzMjkwOTIzLVRYbFFjbTlxIiwibWFpblNlcnZlclVSTCI6Imh0dHBzOi8vbWRscy5keW5hbXNvZnRvbmxpbmUuY29tIiwib3JnYW5pemF0aW9uSUQiOiIxMDMyOTA5MjMiLCJzdGFuZGJ5U2VydmVyVVJMIjoiaHR0cHM6Ly9zZGxzLmR5bmFtc29mdG9ubGluZS5jb20iLCJjaGVja0NvZGUiOjE4NDc0MTg1Mjl9")
@@ -25,6 +28,22 @@ def process_frame(frame):
         
      return results
 
+def barcode_look(input):
+    api_key = "9anv0b5gk9pon5ihsbgbxafo29mi7f"
+    url = "https://api.barcodelookup.com/v3/products?barcode=" + input + "&formatted=y&key=" + api_key
+
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+
+    barcode = data["products"][0]["barcode_number"]
+    print ("Barcode Number: ", barcode, "\n")
+
+    name = data["products"][0]["title"]
+    print ("Title: ", name, "\n")
+
+    print ("Entire Response:")
+    pprint.pprint(data)
+
 
 vc = cv.VideoCapture(0)
 while True:
@@ -39,6 +58,7 @@ while True:
                  cv.line(frame, points[2], points[3], (0,255,0), 2)
                  cv.line(frame, points[3], points[0], (0,255,0), 2)
                  cv.putText(frame, result.barcode_text, points[0], cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255))
+                 barcode_look(result.barcode_text)
 
      if len(barcodeTasks) < threadn:
          task = pool.apply_async(process_frame, (frame.copy(), ))
@@ -49,3 +69,4 @@ while True:
      if ch == 27:
          break
      
+
