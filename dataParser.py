47dc1b87ff64b3b192090aede15dbe1db4301a88
Honkingbruh <101962901+Honkingbruh@users.noreply.github.com>
2024-10-20 19:21:12 -0700

    added a way to add a new product but user needs to close and reopen the file before it updates

--- dataParser.py
+++ dataParser.py
@@ -9,7 +9,12 @@ def getProductsInCategoryName(categoryName):
 def getNameOfCategoryNum(category):
     return df.at[df.query('Category == ' + str(category)).index[0], 'CategoryNames']
 def getNumOfCategoryName(categoryName):
-    return df.at[df.query('CategoryNames == \'' + categoryName + '\'').index[0], 'Category']
+    try:
+        return df.at[df.query('CategoryNames == \'' + categoryName + '\'').index[0], 'Category']
+    except:
+        return getLastCategoryNum() + 1
+def getLastCategoryNum():
+    return df['Category'].max()
 def getBestOfCategory(category):
     if not df.query('Category == ' + str(category) + ' and Rating == \'A\'').empty:
         return df.query('Category == ' + str(category) + ' and Rating == \'A\'')
@@ -23,15 +28,26 @@ def getBestOfCategory(category):
         return df.query('Category == ' + str(category) + ' and Rating == \'F\'')
     if not df.query('Category == ' + str(category) + ' and Rating == \'?\'').empty:
         return df.query('Category == ' + str(category) + ' and Rating == \'?\'')
-#not exact, brand can be a substring of the entire brand name
 def getData(brand):
+    return "Brand: " + brand + "     Category: " + getCategoryNameOf(brand)
+#not exact, brand can be a substring of the entire brand name
+def getLine(brand):
     return df[df['Brand'].str.contains(brand)]
 #brand is the exact brand name, used when user chooses a specific brand when searching and multiple brands with the substring come up
 def getRatingOfSpecific(brand):
     return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'Rating']
 def getPraiseOf(brand):
-    return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'Praise']
+    return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'Praises']
 def getCriticismOf(brand):
-    return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'Criticism']
-#def getAlternative(brand):
-
+    return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'Criticisms']
+def getCategoryOf(brand):
+    return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'Category']
+def getCategoryNameOf(brand):
+    return df.at[df.query('Brand == \'' + str(brand) + '\'').index[0], 'CategoryNames']
+def getAlternative(brand):
+    cat = df.at[getData(brand).index[0], 'Category']
+    return getBestOfCategory(cat)
+def addNewProduct(brand, rating, category, criticism, praise, categoryName):
+    with open('unsorted.csv', 'a') as df:
+        df.write('\n' + brand + ',' + rating + ',' + str(category) + ',' + criticism + ',' + praise + ',' + categoryName)
+        df = pd.read_csv('unsorted.csv')
